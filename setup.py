cd3961828c0a3eee9b64192abf0efaaef004a537
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-08-14 10:50:25 +0200

    Preparing release 0.0.8

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
     OPENSSL_REQUIREMENTS = []
 
 setup(name="PyFxA",
-      version='0.0.8.dev0',
+      version='0.0.8',
       description="Firefox Accounts client library for Python",
       long_description=README + "\n\n" + CHANGES,
       classifiers=[
