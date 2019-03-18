15809edee27311bdef5e5e1f6395a8e6f708478b
Ryan Kelly <ryan@rfk.id.au>
2019-03-18 12:43:01 +1100

    Prep v0.7.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if sys.version_info < (2, 7, 9):
 
 
 setup(name="PyFxA",
-      version='0.6.0',
+      version='0.7.0',
       description="Firefox Accounts client library for Python",
       long_description=README + "\n\n" + CHANGES,
       classifiers=[
