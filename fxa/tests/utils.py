00f0e0fe429d42b5da1a67b154fab65b6ec034f8
Tomáš Chvátal <tchvatal@suse.com>
2019-04-24 13:54:42 +0200

    Fix flake8 import warnings

--- fxa/tests/utils.py
+++ fxa/tests/utils.py
@@ -1,11 +1,10 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
-import sys
 import time
 import random
 import requests
-import unittest
+import unittest # NOQA
 from binascii import unhexlify
 from six import binary_type
 from six.moves.urllib.parse import urlparse, urljoin
